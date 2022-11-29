3e571941a87668f2d291ea1e1686af2df7b06c23
unknown <dgoodenough@proton.me>
2022-11-28 19:25:10 -0800

    set state when inputs change

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,62 +1,81 @@
 import { Form, InputGroup, Col, Row } from 'react-bootstrap';
+import { useState } from 'react';
 
-const SearchBar = () => (
-  <Row>
-    <Col md={{ span: 6, offset: 3 }}>
-      <Form>
-        <Form.Group>
-          <InputGroup>
-            <Form.Control
-              type="text"
-              placeholder="Search to find articles..."
-            />
-            <InputGroup.Text role="button" type="submit">
-              Search
-            </InputGroup.Text>
-          </InputGroup>
-        </Form.Group>
-        <Row className="pt-3">
-          <Col md={4}>
-            <Form.Group>
-              <InputGroup>
-                <Form.Select aria-label="Default select example">
-                  <option>Language</option>
-                  <option value="en">English</option>
-                </Form.Select>
-              </InputGroup>
-            </Form.Group>
-          </Col>
-          <Col md={4}>
-            <Form.Group>
-              <InputGroup>
-                <Form.Select aria-label="Default select example">
-                  <option>Reading Level</option>
-                  <option value="beginner">Beginner</option>
-                  <option value="intermediate">Intermediate</option>
-                  <option value="Advanced">Advanced</option>
-                </Form.Select>
-              </InputGroup>
-            </Form.Group>
-          </Col>
-          <Col md={4}>
-            <Form.Group>
-              <InputGroup>
-                <Form.Select aria-label="Default select example">
-                  <option>Sort By</option>
-                  <option value="beginnerToAdvanced">
-                    Beginner - Advanced
-                  </option>
-                  <option value="advancedToBeginner">
-                    Advanced - Beginner
-                  </option>
-                </Form.Select>
-              </InputGroup>
-            </Form.Group>
-          </Col>
-        </Row>
-      </Form>
-    </Col>
-  </Row>
-);
+const SearchBar = () => {
+  const [query, setQuery] = useState('');
+  const [language, setLanguage] = useState('');
+  const [readingLevel, setReadingLevel] = useState('');
+  const [sortMethod, setSortMethod] = useState('');
+
+  return (
+    <Row>
+      <Col md={{ span: 6, offset: 3 }}>
+        <Form>
+          <Form.Group>
+            <InputGroup>
+              <Form.Control
+                onChange={(e) => setQuery(e.target.value)}
+                type="text"
+                placeholder="Search to find articles..."
+              />
+              <InputGroup.Text role="button" type="submit">
+                Search
+              </InputGroup.Text>
+            </InputGroup>
+          </Form.Group>
+          <Row className="pt-3">
+            <Col md={4}>
+              <Form.Group>
+                <InputGroup>
+                  <Form.Select
+                    onChange={(e) => setLanguage(e.target.value)}
+                    aria-label="Default select example"
+                  >
+                    <option>Language</option>
+                    <option value="en">English</option>
+                  </Form.Select>
+                </InputGroup>
+              </Form.Group>
+            </Col>
+            <Col md={4}>
+              <Form.Group>
+                <InputGroup>
+                  <Form.Select
+                    onChange={(e) => setReadingLevel(e.target.value)}
+                    aria-label="Default select example"
+                  >
+                    <option>Reading Level</option>
+
+                    <option value="beginner">Beginner</option>
+                    <option value="intermediate">Intermediate</option>
+                    <option value="advanced">Advanced</option>
+                  </Form.Select>
+                </InputGroup>
+              </Form.Group>
+            </Col>
+            <Col md={4}>
+              <Form.Group>
+                <InputGroup>
+                  <Form.Select
+                    onChange={(e) => setSortMethod(e.target.value)}
+                    aria-label="Default select example"
+                  >
+                    <option>Sort By</option>
+                    <option value="beginnerToAdvanced">
+                      Beginner - Advanced
+                    </option>
+                    <option value="advancedToBeginner">
+                      Advanced - Beginner
+                    </option>
+                  </Form.Select>
+                </InputGroup>
+              </Form.Group>
+            </Col>
+          </Row>
+        </Form>
+      </Col>
+    </Row>
+  );
+};
 
 export default SearchBar;
