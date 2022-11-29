af94b1430d495e087f4b69dc2d14b0e45706e175
unknown <dgoodenough@proton.me>
2022-11-28 19:15:18 -0800

    Initial layout of search Bar and dropdowns

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,62 @@
-import React from 'react';
+import { Form, InputGroup, Col, Row } from 'react-bootstrap';
 
-const SearchBar = () => <div>SearchBar</div>;
+const SearchBar = () => (
+  <Row>
+    <Col md={{ span: 6, offset: 3 }}>
+      <Form>
+        <Form.Group>
+          <InputGroup>
+            <Form.Control
+              type="text"
+              placeholder="Search to find articles..."
+            />
+            <InputGroup.Text role="button" type="submit">
+              Search
+            </InputGroup.Text>
+          </InputGroup>
+        </Form.Group>
+        <Row className="pt-3">
+          <Col md={4}>
+            <Form.Group>
+              <InputGroup>
+                <Form.Select aria-label="Default select example">
+                  <option>Language</option>
+                  <option value="en">English</option>
+                </Form.Select>
+              </InputGroup>
+            </Form.Group>
+          </Col>
+          <Col md={4}>
+            <Form.Group>
+              <InputGroup>
+                <Form.Select aria-label="Default select example">
+                  <option>Reading Level</option>
+                  <option value="beginner">Beginner</option>
+                  <option value="intermediate">Intermediate</option>
+                  <option value="Advanced">Advanced</option>
+                </Form.Select>
+              </InputGroup>
+            </Form.Group>
+          </Col>
+          <Col md={4}>
+            <Form.Group>
+              <InputGroup>
+                <Form.Select aria-label="Default select example">
+                  <option>Sort By</option>
+                  <option value="beginnerToAdvanced">
+                    Beginner - Advanced
+                  </option>
+                  <option value="advancedToBeginner">
+                    Advanced - Beginner
+                  </option>
+                </Form.Select>
+              </InputGroup>
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
+    </Col>
+  </Row>
+);
 
 export default SearchBar;
