22a83c8582a42f68b616d73d6c8c68f063c9f3d0
Alex Bogut <alexbogut11@gmail.com>
2022-11-30 01:20:09 -0500

    replaces nav dropdowns with nav links to history and saved pages

---
+++ src/components/HistoryListItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const HistoryListItem = ({ article }) => (
+  <Container>
+    <Row className="justify-content-md-center">
+      <Col xs lg="2">
+        <img src={article.image} alt="" width="200" height="200" />
+      </Col>
+      <Col md="auto">
+        <p>{article.title}</p>
+        <p>{article.author}</p>
+        <p>{article.description}</p>
+      </Col>
+      <Col xs lg="2">
+        <div>
+          <span>{article.begginerWords}</span>
+        </div>
+        <div>
+          <span>{article.intermediateWords}</span>
+        </div>
+        <div>
+          <span>{article.advancedWords}</span>
+        </div>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default HistoryListItem;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,15 @@
 import { Switch, Route } from 'react-router-dom';
-
+import Saved from './Saved';
 import Home from './Home';
 import Article from '../containers/Article';
+import History from './History';
 
 const Main = () => (
   <Switch>
     <Route exact path="/" render={() => <Home />} />
     <Route path="/article/:articleId" render={() => <Article />} />
+    <Route path="/saved" render={() => <Saved />} />
+    <Route path="/history" render={() => <History />} />
   </Switch>
 );
 
