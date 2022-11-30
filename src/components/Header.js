86e47ac8e16c3c200c0530805106a956ab64cbc5
Alex Bogut <alexbogut11@gmail.com>
2022-11-29 22:29:36 -0500

    adds initial navbar and DropdownListItem component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,50 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const Header = () => <div>Header</div>;
-
+const Header = () => (
+  <Navbar className="color-nav" expand="lg" variant="light">
+    <Container>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto" alignRight>
+          <NavDropdown
+            align="end"
+            title={<strong>Saved</strong>}
+            id="dropdown-menu-align-end"
+          >
+            <NavDropdown.Item href="#action/3.1">
+              Action Action Action Action Action Action Action Action Action
+              Action Action Action Action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown align="end" title="History" id="dropdown-menu-align-end">
+            <NavDropdown.Item href="#action/3.1">
+              Action Action Action Action Action Action Action Action Action
+              Action Action Action Action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
 export default Header;
