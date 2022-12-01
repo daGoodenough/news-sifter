db6111d731764e59fafbd0455a060d13defa955f
Alex Bogut <alexbogut11@gmail.com>
2022-12-01 16:40:04 -0500

    saved property persists across all pages

--- src/components/SavedListItem.js
+++ src/components/SavedListItem.js
@@ -1,10 +1,26 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { addHistory, addSaved, removeSaved } from '../actions';
 
 const SavedListItem = ({ id, history }) => {
+  const [isSaved, setIsSaved] = useState(true);
+  const dispatch = useDispatch();
+  const savedStories = useSelector((state) => state.saved);
   const clickHandler = () => {
+    dispatch(addHistory(stories[id]));
     history.push(`/${stories[id].id}`);
+  };
+
+  const handleSaveClick = () => {
+    if (Object.hasOwn(savedStories, id)) {
+      dispatch(removeSaved(stories[id]));
+      setIsSaved(false);
+    } else {
+      dispatch(addSaved(stories[id]));
+      setIsSaved(true);
+    }
   };
 
   const stories = useSelector((state) => state.saved);
@@ -31,7 +47,20 @@ const SavedListItem = ({ id, history }) => {
           <p>{stories[id].description}</p>
         </td>
         <td>
-          <img src={stories[id].image} alt="article header" />
+          <img
+            onClick={clickHandler}
+            src={stories[id].image}
+            alt="article header"
+          />
+        </td>
+        <td>
+          <i
+            onClick={handleSaveClick}
+            className="fa-solid fa-star"
+            style={{
+              color: isSaved ? 'yellow' : '',
+            }}
+          />
         </td>
       </tr>
     </tbody>
--- src/containers/Article.js
+++ src/containers/Article.js
@@ -1,20 +1,36 @@
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
 import _ from 'lodash';
+import { addSaved, removeSaved } from '../actions';
 
 const Article = (props) => {
   const stories = useSelector((state) => state.articles);
+  const savedStories = useSelector((state) => state.saved);
   const thisURL = window.location.href;
   const id = parseInt(thisURL.substring(thisURL.lastIndexOf('/') + 1));
   const thisArticle = _.find(stories, (element) => element.id === id);
+  const dispatch = useDispatch();
   console.log(thisArticle);
+
+  const handleSaveClick = () => {
+    if (Object.hasOwn(savedStories, thisArticle.id)) {
+      dispatch(removeSaved(thisArticle));
+    } else {
+      dispatch(addSaved(thisArticle));
+    }
+  };
 
   return (
     <div>
       <h5 className="back-button">
         <Link to="/">Back</Link>
       </h5>
-      <h5 className="save-button">Save</h5>
+      {Object.hasOwn(savedStories, thisArticle.id) ? null : (
+        <h5 onClick={handleSaveClick} className="save-button">
+          Save
+        </h5>
+      )}
       <div className="article-box">
         <h2>{thisArticle.title}</h2>
         <a href={thisArticle.url}>{thisArticle.url}</a>
