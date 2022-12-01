fc56d6263ee21834de98e6904ff6c64f06bd226c
Jack Mullinkosson <jackmullinkosson@Jacks-MacBook-Pro.local>
2022-12-01 15:04:06 -0600

    adjusted what is considered an advanced word

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -33,21 +33,35 @@ async function getWordList() {
   return wordList;
 }
 
+async function getNewWords() {
+  const lemmas = [];
+  const wordForms = [];
+  const response = await fetch('ANCallcount.csv');
+  const data = await response.text();
+  const table = data.split(/\r?\n/).slice(1);
+  table.forEach((row) => {
+    const columns = row.split(',');
+    const lemma = columns[1];
+    const wordForm = columns[0];
+    lemmas.push(lemma);
+    wordForms.push(wordForm);
+  });
+  const wordList = {};
+  wordList.lemmas = lemmas;
+  wordList.wordForms = wordForms;
+  return wordList;
+}
+
 const SearchBar = () => {
   const [query, setQuery] = useState('');
 
   let wordList = {};
 
-  // useEffect(() => {
-  //   wordList = getWordList();
-  // }, []);
-
   const dispatch = useDispatch();
 
   const handleSubmitClick = async () => {
-    wordList = getWordList();
+    wordList = getNewWords();
     const storiesData = await fetchStories(query, await wordList);
-    console.log(storiesData);
     dispatch(addStories(storiesData));
   };
 
