c39989124e8b383575c921fe26ab3b0210f4795a
Damian Goodenough <dgoodenough@proton.me>
2022-12-02 08:56:32 -0800

    On mount make the intial api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-// import { useDispatch, useSelector } from 'react-redux';
-// import React, { useEffect, useState, useRef } from 'react';
-// import { fetchStories } from './actions';
+import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { fetchStories } from './helpers/fetchStoryData';
 import SearchBar from './components/SearchBar';
 import Header from './components/Header';
 import Main from './components/Main';
+import { getNewWords, getCocaWords } from './helpers/getWords';
+import { addStories } from './actions';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const initialLoad = async () => {
+    // setIsLoading(true);
+    const wordList = getNewWords();
+    const cocaWords = getCocaWords();
+    const storiesData = await fetchStories(
+      'news',
+      await wordList,
+      await cocaWords
+    );
+
+    dispatch(addStories(storiesData));
+    // setIsLoading(false);
+  };
+
+  useEffect(() => {
+    initialLoad();
+  }, []);
+
   // const [query, setQuery] = useState('');
 
   // const stories = useSelector((state) => state);
