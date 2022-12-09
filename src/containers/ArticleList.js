185d26cbb3a974a7d41905004adb1f1a8730155f
Damian Goodenough <dgoodenough@proton.me>
2022-12-09 12:36:54 -0800

    Render error page if no articles in store

--- src/containers/ArticleList.js
+++ src/containers/ArticleList.js
@@ -4,11 +4,16 @@ import _ from 'lodash';
 
 import ArticleListItem from './ArticleListItem';
 import sortArticles from '../helpers/sortArticleData';
+import ErrorPage from '../components/ErrorPage';
 /* eslint-disable react/prop-types */
 
 const ArticleList = ({ history }) => {
   const stories = useSelector((state) => state.articles);
   const sort = useSelector((state) => state.sort);
+
+  if (stories === null) {
+    return <ErrorPage />;
+  }
 
   const sortedArticles = sortArticles(stories, sort);
 
