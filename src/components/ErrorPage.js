3e714c08c7675ff43c47c5152c911fecda89cc26
Damian Goodenough <dgoodenough@proton.me>
2022-12-09 12:36:01 -0800

    Create a component to show errors

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,15 @@
+import { Row, Col } from 'react-bootstrap';
+
+const ErrorPage = () => (
+  <Row className="">
+    <Col className="text-center">
+      <div>
+        <h2>OOPS! An error has occurred</h2>
+        <h4>Possible reasons:</h4>
+        <h5>- You do not have a valid API key</h5>
+        <h6>You can try the search again...</h6>
+      </div>
+    </Col>
+  </Row>
+);
+export default ErrorPage;
