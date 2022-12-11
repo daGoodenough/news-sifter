298837f5511450295818deff2d13b63a7ee43fa7
Damian Goodenough <dgoodenough@proton.me>
2022-12-11 15:07:35 -0800

    only return error page if something goes wrong

--- src/containers/ArticleList.js
+++ src/containers/ArticleList.js
@@ -11,11 +11,7 @@ import NoResults from '../components/NoResults';
 const ArticleList = ({ history }) => {
   const stories = useSelector((state) => state.articles);
   const sort = useSelector((state) => state.sort);
-  if (stories === null) {
-    return <NoResults />;
-  }
-
-  if (stories === undefined) {
+  if (!stories) {
     return <ErrorPage />;
   }
 
